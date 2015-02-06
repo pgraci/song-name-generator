560eae7a46d67a7eaaf064e773c9d7f14d6dc102
TriAgency <phil@triagency.com>
2015-02-06 15:49:58 -0500

    #1 - change JS to accept JSON - fix error

--- js/song-name-generator.js
+++ js/song-name-generator.js
@@ -1,10 +1,15 @@
-$(document).ready(function() {
+$(document).ready(function() { 
+    // bind form using ajaxForm
     $('#song-name-form').ajaxForm({
+        // dataType identifies the expected content type of the server response
         dataType:  'json',
+
+        // success identifies the function to invoke when the server response
+        // has been received
         success:   getsongname
     });
 });
 
-function getsongname(data) { 
+function getsongname(data) {
   $('#new-song-name').html(data.songname);
 }
