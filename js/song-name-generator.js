06f6599e3656c501bf024802a97cb863b82f8d6d
TriAgency <phil@triagency.com>
2015-02-06 15:48:00 -0500

    #1 - change JS to accept JSON

--- js/song-name-generator.js
+++ js/song-name-generator.js
@@ -1,5 +1,10 @@
 $(document).ready(function() {
-    $('#song-name-form').ajaxForm(function(data) {
-        $('#new-song-name').html(data);
+    $('#song-name-form').ajaxForm({
+        dataType:  'json',
+        success:   getsongname
     });
 });
+
+function getsongname(data) { 
+  $('#new-song-name').html(data.songname);
+}
